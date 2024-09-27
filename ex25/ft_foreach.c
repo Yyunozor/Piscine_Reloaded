da0c51ba05cc7887c6d6301545e22febc8a5763d
Yyunozor <yyunozor@icloud.com>
2024-09-27 23:24:02 +0200

    25 26

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -6,7 +6,26 @@
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 17:12:00 by anpayot           #+#    #+#             */
-/*   Updated: 2024/09/27 17:12:02 by anpayot          ###   ########.fr       */
+/*   Updated: 2024/09/27 22:37:55 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_putnbr(int n);
+
+void	ft_foreach(int *tab, int length, void (*f)(int))
+{
+	while (length--)
+		f(*tab++);
+}
+
+/*
+int	main(void)
+{
+	int		tab[] = {1, 2, 3, 4, 5};
+	void	(*f)(int);
+
+	f = &ft_putnbr;
+	ft_foreach(tab, 5, f);
+	return (0);
+}
+*/
---
+++ ex26/ft_count_if.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 23:08:19 by anpayot           #+#    #+#             */
+/*   Updated: 2024/09/27 23:22:48 by anpayot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_count_if(char **tab, int (*f)(char*))
+{
+	int	count;
+
+	count = 0;
+	while (*tab != 0)
+	{
+		if (f(*tab) == 1)
+			count++;
+		tab++;
+	}
+	return (count);
+}
+/*
+#include <stdio.h>
+int	starts_with_a(char *str)
+{
+	if (str[0] == 'a')
+		return (1);
+	return (0);
+}
+int	starts_with_b(char *str)
+{
+	if (str[0] == 'b')
+		return (1);
+	return (0);
+}
+int	ft_strlen(char *str)
+{
+	int	i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+int	ft_len_five(char *str)
+{
+	if (ft_strlen(str) == 5)
+		return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	char	*array[] = {"allo!", "banana", "avocado", "boring", 
+						"segfault", "bus error", "arbitre", "arbre", 0};
+	int		counta = ft_count_if(array, &starts_with_a);
+	int		countb = ft_count_if(array, &starts_with_b);
+	int		countlen = ft_count_if(array, &ft_len_five);
+	printf("Number of strings starting with 'a': %d\n", counta);
+	printf("Number of strings starting with 'b': %d\n", countb);
+	printf("Number of strings with 5 characters: %d\n", countlen);
+	return (0);
+}
+*/
